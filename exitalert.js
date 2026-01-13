06a1474520f225ef5d61287ae82601ae05310b98
Josué Mena <josuemenapro@gmail.com>
2026-01-12 20:06:56 -0600

    Actualizar exitalert.js

--- exitalert.js
+++ exitalert.js
@@ -2,5 +2,24 @@ function confirmarSalida(e) {
     e.preventDefault();
     e.returnValue = "";
 }
+//bienvenida
+window.addEventListener("load", () => {
+  const splash = document.getElementById("splash");
+  const contenido = document.getElementById("contenido");
+
+  if (!splash || !contenido) return;
+
+  // aseguramos que el splash se renderice primero
+  requestAnimationFrame(() => {
+    setTimeout(() => {
+      splash.classList.add("fade-out");
+
+      setTimeout(() => {
+        splash.style.display = "none";
+        contenido.classList.remove("hidden");
+      }, 1000);
+    }, 1800);
+  });
+});
 
 window.addEventListener("beforeunload", confirmarSalida);
