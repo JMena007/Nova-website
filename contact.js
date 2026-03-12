c8b5e3bec7280dbde745c0885af80a0deb757961
Josué Mena <josuemenapro@gmail.com>
2026-03-12 11:48:35 -0600

    Actualizar contact.js
    aplicación de animación de carga

--- contact.js
+++ contact.js
@@ -1,4 +1,33 @@
 
+// splash anumation
+window.addEventListener("load", () => {
+  const splash = document.getElementById("splash-contact");
+  const contenido = document.getElementById("contenido");
+
+  if (!splash || !contenido) return;
+
+  const skipSplash = sessionStorage.getItem("skipSplash");
+
+  if (skipSplash) {
+    splash.style.display = "none";
+    contenido.classList.add("show");
+    sessionStorage.removeItem("skipSplash");
+    return;
+  }
+
+  // Espera 1.8s antes de iniciar fade
+  setTimeout(() => {
+    splash.classList.add("fade-out");  // splash comienza fade
+    contenido.classList.add("show");   // contenido aparece suavemente
+
+    // Después de que termine la transición, ocultar splash
+    setTimeout(() => {
+      splash.style.display = "none";
+    }, 800); // igual al tiempo de transición en CSS
+  }, 1800);
+});
+
+
 /* ==========================
    Efecto fade-out al salir de la página de contacto
 ========================== */
